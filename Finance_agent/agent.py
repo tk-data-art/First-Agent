fac62313f9c5711f5ece8ce6a14e32fe9ddb90bd
Your NameTholkappian <tholkappian0608@gmail.com>
2026-02-24 00:21:53 +0400

    Initial commit for Finance Agent

---
+++ Finance_agent/agent.py
@@ -0,0 +1,15 @@
+from google.adk.agents import LlmAgent
+from google.adk.tools import google_search # Corrected path
+
+finance_assistance_agent = LlmAgent(
+    name= "finance_assistance_agent",
+    model="gemini-2.5-flash",
+    description= "A simple finance assistant that helps with user's finance goals.",
+    instruction= """You are a friendly assistant.
+    You can help user's generic questions on finance and help plan their finance goals.
+    Be more friendly and positive.""",
+
+    tools= [google_search],
+)
+
+root_agent = finance_assistance_agent
